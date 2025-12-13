dc6772fb7b3a4e537a535a087ddea429b014f09c
SechabaVDM <162897708+SechabaVDM@users.noreply.github.com>
2025-12-13 22:13:02 +0200

    .

--- GADE_2B_1/Source/GADE_2B_1/GADE_2B_1.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GADE_2B_1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GADE_2B_1, "GADE_2B_1" );
